9be6eee7df319d4daa3b68118e64a02d2f078ede
Eduardo Vargas <eduardovargas21@ciencias.unam.mx>
2026-09-05 18:39:06 -0600

    Practica 02: Django, completada

---
+++ perfil/static/perfil/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const boton = document.getElementById("btn-saludo");
+    boton.addEventListener("click", function () {
+        alert("¡Gracias por visitar mi perfil de Ciencias de la Computación!");
+    });
+});
